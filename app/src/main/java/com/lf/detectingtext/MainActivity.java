912b90c893b62da3aa79bdf63fcf2b73bee74f22
Lucas <frontalinilucas@gmail.com>
2017-04-10 18:28:59 -0300

    Select gallery image

--- app/src/main/java/com/lf/detectingtext/MainActivity.java
+++ app/src/main/java/com/lf/detectingtext/MainActivity.java
@@ -1,13 +1,81 @@
 package com.lf.detectingtext;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatImageView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_IMAGE = 501;
+
+    @BindView(R.id.btnSelectImage)
+    AppCompatButton mBtnSelectImage;
+
+    @BindView(R.id.imageSelected)
+    AppCompatImageView mImageSelected;
+
+    @BindView(R.id.fab)
+    FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mBtnSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, getString(R.string.selectImage)), PICK_IMAGE);
+            }
+        });
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    //TODO: savedInstanceState
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK){
+            switch(requestCode){
+                case PICK_IMAGE:
+                    Uri uri = data.getData();
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                        mImageSelected.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.errorSelectImage), Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }
     }
 }
